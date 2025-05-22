19663d42c8d3e754f3aab63ea4935529bbf9e95b
PedroVasPassos <pdvasconcelos01@gmail.com>
2025-05-22 11:15:00 -0300

    UPDATE: _layout.jsx
    poder fazer as telas de erro

--- fokus-base/app/_layout.jsx
+++ fokus-base/app/_layout.jsx
@@ -1,7 +1,10 @@
 import { Stack } from 'expo-router';
+import { RootSiblingParent } from 'react-native-root-siblings';
 
 export default function Layout() {
   return (
-    <Stack screenOptions={{ headerShown: false }} />
+    <RootSiblingParent>
+      <Stack screenOptions={{ headerShown: false }} />
+    </RootSiblingParent>
   );
 }
