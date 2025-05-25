260ae18c72f7cdff1f9325d51ad729cbbc111bba
PedroVasPassos <pdvasconcelos01@gmail.com>
2025-05-25 01:11:40 -0300

    CREATE: config.js
    mudar o IP por aqui q ira mudar para todos

---
+++ fokus-base/app/utils/config.js
@@ -0,0 +1,7 @@
+export const BASE_URL = 'http://192.168.0.66:3001'; // atualize o IP e porta quando mudar
+
+export const CARDAPIO_URL = `${BASE_URL}/cardapio`;
+export const CADASTRO_URL = `${BASE_URL}/cadastro`;
+export const LOGIN_URL = `${BASE_URL}/login`;
+export const PEDIDO_URL = `${BASE_URL}/pedido`;
+export const PEDIDO_GET_URL = `${BASE_URL}/pedidos`;
