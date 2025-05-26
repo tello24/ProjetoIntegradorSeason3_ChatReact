bb36a3392dad0954d9c0ea91b78a74721e52faa2
PedroVasPassos <pdvasconcelos01@gmail.com>
2025-05-26 14:08:34 -0300

    UPDATE: config.js

--- fokus-base/app/utils/config.js
+++ fokus-base/app/utils/config.js
@@ -4,4 +4,4 @@ export const CARDAPIO_URL = `${BASE_URL}/cardapio`;
 export const CADASTRO_URL = `${BASE_URL}/cadastro`;
 export const LOGIN_URL = `${BASE_URL}/login`;
 export const PEDIDO_URL = `${BASE_URL}/pedido`;
-export const PEDIDO_GET_URL = `${BASE_URL}/pedidos`;
+export const PEDIDO_GET_URL = `${BASE_URL}/pedido`;
