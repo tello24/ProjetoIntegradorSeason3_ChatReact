12893e339d3d0437bc31f2cabfb9f79779103e31
PedroVasPassos <pdvasconcelos01@gmail.com>
2025-06-11 21:15:50 -0300

    UPDATE: config.js
    colocando o cancelar pedido na URL

--- fokus-base/app/utils/config.js
+++ fokus-base/app/utils/config.js
@@ -1,7 +1,8 @@
-export const BASE_URL = 'http://10.2.1.165:3001'; // atualize o IP e porta quando mudar 'http://<YOUR_IP_ADRESS>:3001'
+export const BASE_URL = 'http://192.168.0.66:3001'; // atualize o IP e porta quando mudar 'http://<YOUR_IP_ADRESS>:3001'
 
 export const CARDAPIO_URL = `${BASE_URL}/cardapio`;
 export const CADASTRO_URL = `${BASE_URL}/cadastro`;
 export const LOGIN_URL = `${BASE_URL}/login`;
 export const PEDIDO_URL = `${BASE_URL}/pedido`;
 export const PEDIDO_GET_URL = `${BASE_URL}/pedido`;
+export const CANCELAR_PEDIDO_URL = `${BASE_URL}/pedidos`;
